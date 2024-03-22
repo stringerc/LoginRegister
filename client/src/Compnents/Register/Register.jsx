409d1532730127912a36d13309e1363935a35147
stringerc <stringer.c.a@gmail.com>
2024-03-22 14:58:45 -0400

    add mySQL database

--- client/src/Compnents/Register/Register.jsx
+++ client/src/Compnents/Register/Register.jsx
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './Register.scss'
 import '../../App.css'
 import { Link } from 'react-router-dom'
+import Axios from 'axios'
 
 //Import our Assets
 import video from '../../LoginAssets/video.mp4'
@@ -14,6 +15,24 @@ import {AiOutlineSwapRight} from 'react-icons/ai'
 import {MdMarkEmailRead} from 'react-icons/md'
 
 const Register = () => {
+//UseState to hold our inputs
+    const [email, setEmail] = useState('')
+    const [userName, setUserName] = useState('')
+    const [password, setPassword] = useState('')
+
+// Onclick let us get what the user has entered
+const createUser = ()=>{
+    //We shall require Axios to create an API that connects to the server - lets install that
+    Axios.post('http://localhost:3002/register', {
+        // create variable to send to the server through the route
+        Email: email,
+        Username: userName,
+        Password: password
+    }). then(()=>{
+        console.log('User has been created')
+    })
+}
+
   return (
     <div className="registerPage flex">
         <div className="container flex">
@@ -47,7 +66,9 @@ const Register = () => {
                         <label htmlFor="email">Email</label>
                         <div className="input flex">
                             <MdMarkEmailRead className='icon' />
-                            <input type="email" id='username' placeholder='Enter Email' />
+                            <input type="email" id='username' placeholder='Enter Email' onChange={(event)=>{
+                                setEmail(event.target.value)
+                            }}/>
 
                         </div>
                     </div>
@@ -57,7 +78,9 @@ const Register = () => {
                         <label htmlFor="usename">Username</label>
                         <div className="input flex">
                             <FaUserShield className='icon' />
-                            <input type="text" id='username' placeholder='Enter Username' />
+                            <input type="text" id='username' placeholder='Enter Username' onChange={(event)=>{
+                                setUserName(event.target.value)
+                            }}/>
 
                         </div>
                     </div>
@@ -66,11 +89,13 @@ const Register = () => {
                         <label htmlFor="password">Password</label>
                         <div className="input flex">
                             <BsFillShieldLockFill className='icon' />
-                            <input type="password" id='password' placeholder='Enter Password' />
+                            <input type="password" id='password' placeholder='Enter Password' onChange={(event)=>{
+                                setPassword(event.target.value)
+                            }}/>
                         </div>
                     </div>
 
-                    <button type='submit' className='btn flex'>
+                    <button type='submit' className='btn flex' onClick={createUser}>
                         <span>Register</span>
                         <AiOutlineSwapRight className='icon'/>
                     </button>
--- server/index.js
+++ server/index.js
@@ -14,6 +14,31 @@ app.listen(3002, ()=>{
 const db = mysql.createConnection({
     user: 'root',
     host: 'localhost',
-    password: '', //If you have set xamp password please enter it here
-    database: '',
+    password: '', //If you have set xampp password please enter it here
+    database: 'plantdb',
+})
+
+// Let us now create a aroute to the server that will register a user.
+
+app.post('/register', (req, res)=>{
+    // We need to get variables sent from the form
+    const sentEmail = req.body.Email
+    const sentUserName = req.body.UserName
+    const sentPassword = req.body.Password
+
+    //Lets create SQL statement to insert the user to the Database table Users
+    const SQL = 'INSERT INTO Users (email, username, password) VALUES (?,?,?)' //We are going to enter these values through a variable
+    const Values = [sentEmail, sentUserName, sentPassword]
+    
+    //Query to execute the sql statement stated above
+    db.query(SQL, Values, (err, resultes)=>{
+        if(err){
+            res.send(err)
+        }
+        else{
+            console.log('User inserted successfully!')
+            res.send({message: 'User added!'})
+            // Lets try and see
+        }
+    })
 })
